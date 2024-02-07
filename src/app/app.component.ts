c0c668feb339daf71ca2754b74df81f8724fba58
Roccofranchini <rocco.franchini1@gmail.com>
2024-02-07 04:47:15 +0100

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+ 
 })
 export class AppComponent {
   title = '14feb';
+
+
+  risposta: string = ""
+
+  saidNo() {
+    this.risposta = "N"
+  }
+  
+  saidYes() {
+    this.risposta = "Y"
+  }
+
+  saidSi() {
+    this.risposta = "S"
+  }
+
+  saidPronta() {
+    this.risposta = "S1"
+  }
+
+  saidS2() {
+    this.risposta = "S2"
+  }
+
+  saidOk() {
+    this.risposta = "ok"
+  }
+  
 }
